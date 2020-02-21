d17d762da7f310cc8df9298b35cfcd22ef87cbe8
behindthebits <behindthebits@gmail.com>
2020-02-20 18:10:31 -0600

    Fixed documentation for @param tag - it is better to use String[] args, so the documentation will have a better format.

--- Driver.java
+++ Driver.java
@@ -14,7 +14,7 @@ public class Driver{
      * @return    nothing
      * @throws    nothing
      */
-    public static void main(String args[])
+    public static void main(String[] args)
     {
         // don't forget to comment your code, so others can understand your logic
         try{
